c013c4bad6d997030204160039811b2167ce8b7f
olenese <74682960+olenese@users.noreply.github.com>
2020-11-18 22:02:25 +0100

    Add files via upload

---
+++ Webscraper/webscrapeperpolestar.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+import smtplib
+
+url = 'https://hotell.difi.no/?dataset=vegvesen/kjoretoy&query=JM1NA3512M1221038'
+browser = webdriver.Chrome("/Users/olenese/Programmering/Python/Webscraper/chromedriver")
+browser.get(url)
+
+browser.find_element_by_xpath('//*[@id="data"]/div[3]/table/tbody/tr/td[3]')
+
+##https://hotell.difi.no/?dataset=vegvesen/kjoretoy&query=JM1NA3512M1221038
