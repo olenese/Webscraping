a43f1bdd09e18e684c95f19ff890e644d855782a
olenese <74682960+olenese@users.noreply.github.com>
2020-11-18 22:06:09 +0100

    Update webscrapeperpolestar.py
    oppdatert understellsnummer

--- Webscraper/webscrapeperpolestar.py
+++ Webscraper/webscrapeperpolestar.py
@@ -5,6 +5,6 @@ url = 'https://hotell.difi.no/?dataset=vegvesen/kjoretoy&query=JM1NA3512M1221038
 browser = webdriver.Chrome("/Users/olenese/Programmering/Python/Webscraper/chromedriver")
 browser.get(url)
 
-browser.find_element_by_xpath('//*[@id="data"]/div[3]/table/tbody/tr/td[3]')
+browser.find_element_by_xpath('//*[@id="data"]/div[3]/table/tbody/tr/td[5]')
 
-##https://hotell.difi.no/?dataset=vegvesen/kjoretoy&query=JM1NA3512M1221038
+##https://hotell.difi.no/?dataset=vegvesen/kjoretoy&query=LPSVSEDEEML014003
